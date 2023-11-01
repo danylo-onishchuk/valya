66a7d2247804730e8882fdda820a1ef21de1f754
unknown <onishchuk.danil@gmail.com>
2023-11-01 19:03:45 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Valya is the best artist in the world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
