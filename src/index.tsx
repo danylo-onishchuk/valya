c96d71ec1d958bddddbeb7d415afe125e85a49d2
unknown <onishchuk.danil@gmail.com>
2023-11-15 15:06:10 +0200

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={''}>
+    <BrowserRouter basename={'valentyna-melnychuck.com'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
