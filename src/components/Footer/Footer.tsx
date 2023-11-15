02571614406a1981f109c859f995a39a9eaf973f
unknown <onishchuk.danil@gmail.com>
2023-11-15 14:37:03 +0200

    add email and insta

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,15 +4,29 @@ import styles from './Footer.module.scss';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-    <div className={styles.icons}>
-      <AiFillInstagram size={32} />
-      <AiFillMail size={32} />
-    </div>
+      <div className={styles.icons}>
+        <a 
+          href="https://www.instagram.com/valentyna_melnychuck" 
+          target="_blanc"
+          className={styles.icon}
+        >
+          <AiFillInstagram size={32} />
+        </a>
+        <a
+          href="https://mail.google.com/mail/?view=cm&fs=1&to=valentinaartislife@gmail.com"
+          target="_blank" 
+          rel="noreferrer"
+          className={styles.iconEmail}
+        >
+          <AiFillMail size={32} />
+        </a>
+      </div>
 
-    <div className={styles.copyright}>
-      <AiOutlineCopyrightCircle />
-      by Valentyna Melnychuck
-    </div>
-  </footer>
+      <div className={styles.copyright}>
+        <AiOutlineCopyrightCircle />
+        by Valentyna Melnychuck
+      </div>
+    </footer>
   );
 }
+
